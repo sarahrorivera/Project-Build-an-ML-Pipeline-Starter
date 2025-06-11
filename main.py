8ee9cc3efe4c2f60aa965bab9794d485bc801238
Sarah Rivera <sriv435@gitlab.com>
2025-06-11 19:42:08 -0400

    Added test_regression_model step to pipeline

--- main.py
+++ main.py
@@ -131,17 +131,15 @@ def go(config: DictConfig):
 
             ##################
             # Implement here #
-            _ = mlflow.run(
-                f"{config['main']['components_repository']}/test_regression_model",
+             _ = mlflow.run(
+                os.path.join(config["main"]["components_repository"], "test_regression_model"),
                 "main",
                 parameters={
-                "model": "random_forest_export:latest",
-                "test_dataset": "test_data.csv:latest"
-        },
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest"
+                }
             )
             ##################
-
-            pass
 
 
 if __name__ == "__main__":
