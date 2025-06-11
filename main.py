35d089301f21333d99f8bf72dd834d2488c7b8a6
Sarah Rivera <sriv435@gitlab.com>
2025-06-11 19:19:25 -0400

    added train random forest and test regression to main.py

--- main.py
+++ main.py
@@ -110,6 +110,19 @@ def go(config: DictConfig):
 
             ##################
             # Implement here #
+            _ = mlflow.run(
+                os.path.join(here, "src", "train_random_forest"),
+                "main",
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "rf_config": rf_config,
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                    "output_artifact": "random_forest_export"
+                },
+            )
             ##################
 
             pass
@@ -118,6 +131,14 @@ def go(config: DictConfig):
 
             ##################
             # Implement here #
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                "main",
+                parameters={
+                "model": "random_forest_export:latest",
+                "test_dataset": "test_data.csv:latest"
+        },
+            )
             ##################
 
             pass
